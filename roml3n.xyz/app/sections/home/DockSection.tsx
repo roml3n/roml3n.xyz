8ecc8f110e7dfdb3a7038b081ee36e005d5a278d
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-11-29 14:15:55 +0300

    style: dock

--- roml3n.xyz/app/sections/home/DockSection.tsx
+++ roml3n.xyz/app/sections/home/DockSection.tsx
@@ -1,66 +1,68 @@
-import React from 'react';
+import React from "react";
 import { FloatingDock } from "@/app/components/ui/floating-dock";
 import {
-    IconHomeFilled,
-    IconPhotoFilled,
-    IconPaletteFilled,
-    IconUserFilled,
-    IconBallpenFilled,
-    IconFlaskFilled,
-  } from "@tabler/icons-react";
+  IconHomeFilled,
+  IconPhotoFilled,
+  IconPaletteFilled,
+  IconUserFilled,
+  IconBallpenFilled,
+  IconFlaskFilled,
+} from "@tabler/icons-react";
+import { cn } from "@/lib/utils";
 
-const DockSection = ({ className } : { className: string }) => {
+const DockSection = ({ className }: { className: string }) => {
   return (
-    <div className={className}>
-        <FloatingDock
-          items={[
-            {
-              title: "Home",
-              icon: (
-                <IconHomeFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
-              ),
-              href: "/home",
-            },
-            {
-              title: "About",
-              icon: (
-                <IconUserFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
-              ),
-              href: "/about",
-            },
-            {
-              title: "Work",
-              icon: (
-                <IconPaletteFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
-              ),
-              href: "/work",
-            },
-            {
-              title: "Writing",
-              icon: (
-                <IconBallpenFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
-              ),
-              href: "/writing",
-            },
-            {
-              title: "Experiments",
-              icon: (
-                <IconFlaskFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
-              ),
-              href: "/experiments",
-            },
-            {
-              title: "Photos",
-              icon: (
-                <IconPhotoFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
-              ),
-              href: "/photos",
-            },
-          ]}
-          desktopClassName="fixed bottom-4 rounded-full !bg-lightgrey border border-solid !border-midgrey"
-        />
+    <div className={cn(className)}>
+      <FloatingDock
+        items={[
+          {
+            title: "Home",
+            icon: (
+              <IconHomeFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
+            ),
+            href: "/home",
+          },
+          {
+            title: "About",
+            icon: (
+              <IconUserFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
+            ),
+            href: "/about",
+          },
+          {
+            title: "Work",
+            icon: (
+              <IconPaletteFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
+            ),
+            href: "/work",
+          },
+          {
+            title: "Writing",
+            icon: (
+              <IconBallpenFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
+            ),
+            href: "/writing",
+          },
+          {
+            title: "Experiments",
+            icon: (
+              <IconFlaskFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
+            ),
+            href: "/experiments",
+          },
+          {
+            title: "Photos",
+            icon: (
+              <IconPhotoFilled className="h-12 w-12 text-darkgrey group-hover:text-mainblue" />
+            ),
+            href: "/photos",
+          },
+        ]}
+        desktopClassName="fixed bottom-4 rounded-full !bg-lightgrey border border-solid !border-midgrey"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default DockSection
+export default DockSection;
+
