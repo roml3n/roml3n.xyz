ae2011025153b8575eafe6d32aeb68d89281a0fb
Roman Lenjo <roman25lenjo@gmail.com>
2024-12-02 23:33:52 +0300

    💧 Add progressive blur on header and footer

--- roml3n.xyz/app/components/Footer.tsx
+++ roml3n.xyz/app/components/Footer.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='w-full relative py-8 -z-10 flex justify-between'>
-        <p className="h4">©2025 Roman Lenjo</p>
-        <p className="h4">Crafted with ❤️ from 🇰🇪</p>
+    <div className="w-full relative py-8 -z-10 flex justify-between mb-28">
+      <p className="h4">©2025 Roman Lenjo</p>
+      <p className="h4">Crafted with ❤️ from 🇰🇪</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
