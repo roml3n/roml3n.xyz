227095478aa85fc8164b568473c64b002f416924
Roman Lenjo <roman25lenjo@gmail.com>
2025-03-07 16:15:45 +0300

    ✅ Fix floating dock and add metadata

--- app/components/ui/floating-dock.tsx
+++ app/components/ui/floating-dock.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { IconLayoutNavbarCollapse } from "@tabler/icons-react";
+// import { IconLayoutNavbarCollapse } from "@tabler/icons-react";
 import {
   AnimatePresence,
   MotionValue,
