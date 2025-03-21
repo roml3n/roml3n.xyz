ad96ab4e142055cdb0ab338536c5a51397abf1d6
Roman Lenjo <roman25lenjo@gmail.com>
2025-03-21 09:28:44 +0300

    get dynamic date

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full relative py-8 -z-10 flex flex-col md:flex-row gap-4 md:justify-between mb-28">
-      <p className="h4">©2025 Roman Lenjo</p>
+      <p className="h4">©{new Date().getFullYear()} Roman Lenjo</p>
       <p className="h4">Crafted with ❤️ from 🇰🇪</p>
     </div>
   );
