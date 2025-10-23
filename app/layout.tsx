254f63165b8027040392d4b20d415bf2bcfc0213
Roman Lenjo <roman25lenjo@gmail.com>
2025-10-23 22:59:46 +0300

    Add workCards to hjomepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import DockSection from "@/app/(homeSections)/DockSection";
+import DockSection from "@/app/(sections)/DockSection";
 import Header from "@/app/components/Header.jsx";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@
 import React from "react";
 import "./globals.css";
 
-import { Hero } from "./(homeSections)/hero";
-import { ProjectsSection } from "./(homeSections)/ProjectsSection";
-import { ToolStackSection } from "./(homeSections)/ToolStackSection";
+import { Hero } from "./(sections)/hero";
+import { ToolStackSection } from "./(sections)/ToolStackSection";
+import WorkSection from "./(sections)/WorkSection";
 import Footer from "@/app/components/Footer";
+import ButtonLink from "@/app/components/ButtonLink";
 
 function App() {
   return (
@@ -15,7 +16,16 @@ function App() {
       <div className="flex flex-col items-center relative">
         <main className="flex flex-col w-full">
           <Hero />
-          <ProjectsSection />
+
+          {/* Project Section */}
+          <div className="mt-16 flex flex-col gap-4 md:gap-6">
+            <div className="w-full flex flex-col md:flex-row justify-between items-center py-0 md:py-4">
+              <h2 className="h2 pb-3 md:pb-0"> Featured Projects </h2>
+              <ButtonLink Label="view all projects" URL="/work" />
+            </div>
+            <WorkSection />
+          </div>
+
           <ToolStackSection />
         </main>
         <Footer />
