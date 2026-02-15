070753534e002917cf2955d0319e2b2b48a80b51
Roman Lenjo <roman25lenjo@gmail.com>
2026-02-15 18:05:41 +0300

    implement hover interaction on WorkCard.tsx

--- app/components/WorkCard.tsx
+++ app/components/WorkCard.tsx
@@ -16,11 +16,12 @@ const WorkCard = ({
 }) => {
   return (
     <div
-      className={`flex flex-col gap-6 col-span-4 md:col-span-4 lg:col-span-6 row-span-1 ${className}`}
+      className={`group relative flex flex-col gap-6 col-span-4 md:col-span-4 lg:col-span-6 row-span-1 ${className}`}
     >
+      <span className="pointer-events-none absolute -inset-3 -z-10 rounded-lg bg-mainblue/20 opacity-0 scale-95 transition-all duration-300 group-hover:opacity-100 group-hover:scale-100" />
       <div className="flex flex-col gap-1">
         <h4 className="h4 text-fullgrey w-full !leading-none">{title}</h4>
-        <h5 className="h5 font-medium w-full opacity-60 !leading-none">
+        <h5 className="h5 font-medium w-full opacity-60 !leading-tight">
           {company}
           <br />[ {date} ]
         </h5>
