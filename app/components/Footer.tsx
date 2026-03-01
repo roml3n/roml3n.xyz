4388af85801af178acd14b0d9efe66d8d080faf2
Roman Lenjo <roman25lenjo@gmail.com>
2026-03-01 18:28:14 +0300

    fix issues with footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="w-full min-h-[34rem] md:min-h-[38rem] overflow-hidden grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 py-10 md:py-12">
-      <div className="col-span-4 md:col-span-8 lg:col-span-12 gap-x-6 flex flex-col -gap-6 h-fit">
+      <div className="col-span-4 md:col-span-8 lg:col-span-12 gap-x-6 flex flex-col -gap-6 h-full">
         <div className="z-20 flex items-start justify-between gap-4 col-span-4 md:col-span-8 lg:col-span-12">
           {/* Logo  */}
           <TransitionLink
@@ -54,13 +54,15 @@ const Footer = () => {
           </div>
         </div>
 
-        <Image
-          src="/images/footer-img.webp"
-          alt="watercolored image of roman sat in a chair listening to music"
-          width={2400}
-          height={2400}
-          className=" pointer-events-none place-self-center flex items-center object-cover"
-        />
+        <div className="relative flex w-[90%] md:w-[60%] lg:w-1/2 h-auto items-center justify-center place-self-center">
+          <Image
+            src="/images/footer-img.webp"
+            alt="watercolored image of roman sat in a chair listening to music"
+            width={2400}
+            height={2400}
+            className="pointer-events-none w-full h-auto object-contain"
+          />
+        </div>
       </div>
       {/* glow effect */}
       {/* <div className="pointer-events-none absolute left-1/2 bottom-0 h-[18rem] w-[180vw] max-w-none -translate-x-1/2 translate-y-1/2 bg-[#33B0D5] rounded-[999px] blur-[100px] z-0" /> */}
